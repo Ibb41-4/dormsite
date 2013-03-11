8808bc9b1231b311468547859fcbfd3a445cf1b1
Hmvp <github@hmvp.nl>
2013-03-11 11:26:46 +0100

    serve static content properly using google drive

--- dormsite/settings.py
+++ dormsite/settings.py
@@ -77,11 +77,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = PROJECT_DIR + '/staticfiles'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = 'https://googledrive.com/host/0B5K79dR3ug0ucGx6d29qSTlQek0/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
